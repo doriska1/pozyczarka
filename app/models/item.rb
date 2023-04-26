fb05b496019fcd0cd3f447e4c4332b2db1d14919
Dorota K <130973186+doriska1@users.noreply.github.com>
2023-04-26 13:07:32 +0200

    Formating project

---
+++ app/models/item.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id         :bigint           not null, primary key
+#  active     :boolean
+#  borrowed   :boolean
+#  item_type  :integer          default("other"), not null
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Item < ApplicationRecord
+  validates :name, presence: true, length: {minimum:3}
+
+  enum item_type: %i[book cd electric_device other]
+end
