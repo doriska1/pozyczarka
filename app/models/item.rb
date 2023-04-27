11cac927eb2ef54636aee922a2bb4bdc9e6442b1
Dorota K <130973186+doriska1@users.noreply.github.com>
2023-04-27 10:55:56 +0200

    Cleaning code (scope, helpers & partials)

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,4 +14,10 @@ class Item < ApplicationRecord
   validates :name, presence: true, length: {minimum:3}
 
   enum item_type: %i[book cd electric_device other]
+
+  scope :active, -> { where(active: true)}
+  scope :inactive, -> { where(active: false)}
+  scope :borrowed, -> { where(borrowed: true)}
+  scope :on_place, -> { where(borrowed: false)}
+
 end
