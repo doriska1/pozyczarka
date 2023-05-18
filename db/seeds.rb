ebe92373016d42eaa2e11b70b865d6f2aae8ff7c
Dorota K <130973186+doriska1@users.noreply.github.com>
2023-05-18 10:26:53 +0200

    Fix rails best practises warnings

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 p "Generating fake items"
 
-20.times do 
-    Item.find_or_create_by(name: Faker::Books::CultureSeries.book, active: true, borrowed: false)
+20.times do
+  Item.find_or_create_by(name: Faker::Books::CultureSeries.book, active: true, borrowed: false)
 end
 
 p "#{Item.count} items generated"
